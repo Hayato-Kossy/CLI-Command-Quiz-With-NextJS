4bdc08dd249d434af62bbcb58019051a1e580d7c
Hayato Koshi <kshyt2501@icloud.com>
2023-01-30 02:53:30 +0900

    feat/adding mui and firebase

--- cli-command-quiz/pages/index.tsx
+++ cli-command-quiz/pages/index.tsx
@@ -3,19 +3,34 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import GoogleIcon from '@mui/icons-material/Google';
-// import { signOut } from 'firebase/auth';
-// import { auth, signIn } from '../src/firebase';
+import { signOut } from 'firebase/auth';
+import { auth, signIn } from '../src/firebase';
 import { useRouter } from 'next/router';
 import { Layout } from '../componets/Layout';
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+
+  const [isAuth, setIsAuth] = React.useState(false);
+
+  const logIn = async () => {
+    await signIn();
+    // router.push(`/mypage/${localStorage.getItem('userId')}`);
+    setIsAuth(true);
+  };
+  const logOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+    });
+  };
   return (
     <Layout title='Title'>
       <Container maxWidth='lg'>
         <Box
-          height='50vh'
+          height='80vh'
           sx={{
             my: 4,
             display: 'flex',
@@ -26,14 +41,13 @@ export default function Home() {
         >
           <Box sx={{ 
             typography: 'h1', 
-            fontWeight: 900,
+            fontWeight: 400,
           }}
           style={{
             color:'#28a745',
           }}
           >
-            {/* フォントの適用に失敗 */}
-            <a className='font-logo'>CLIコマンドクイズ</a>
+            <a style={{fontFamily:"Arial Black"}}>CLI Command Quiz</a>
           </Box>
           <Box
             sx={{
@@ -51,7 +65,7 @@ export default function Home() {
             size='large'
             color='success'
             >
-              ファイルディレクトリクイズ
+              <a style={{fontFamily:"Impact"}}>ファイルディレクトリクイズ</a>
             </Button>
             <Button 
             sx={ { 
@@ -65,11 +79,11 @@ export default function Home() {
               フリーモード
             </Button>
           </Box>
-          {/* <Box maxWidth='sm' sx={{ py: 2 }}>
+          <Box maxWidth='sm' sx={{ marginTop: 5, }}>
             {!isAuth ? (
               <Button onClick={logIn} variant='outlined' size='large'>
                 <GoogleIcon />
-                Login/Signin
+                Login
               </Button>
             ) : (
               <Button onClick={logOut} variant='outlined' size='large'>
@@ -77,7 +91,7 @@ export default function Home() {
                 Logout
               </Button>
             )}
-          </Box> */}
+          </Box>
         </Box>
       </Container>
     </Layout>  
